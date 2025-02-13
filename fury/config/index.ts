854c980bc82f758ebb2e19dc5ee72731e5818ce8
nyel-101 <dandevjr@gmail.com>
2025-02-13 16:41:42 +0000

    docs(core): restructure database index

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8beb5789250a451e8fd90c5fa2d843ee = () => {
-  return '2026-06-17T11:35:27Z';
+const node_05e3a2f5484042c79a4a2ccb24d486fa = () => {
+  return '2025-02-13T16:41:42Z';
 };
