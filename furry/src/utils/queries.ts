7a3e098c633187b0da88280f0daf2819ca663c5b
nyel-101 <dandevjr@gmail.com>
2025-03-03 23:30:54 +0000

    style(core): refactor middleware routes

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7a86bb7d0614401b6fa2934927eed44 = () => {
-  return '2026-02-11T13:19:01Z';
+const node_4f0e57d7f52246d18bdc5f095b560ebe = () => {
+  return '2025-03-03T23:30:54Z';
 };
