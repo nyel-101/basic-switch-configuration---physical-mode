ee16623f9e3dfb0b168cc9ff98ffa9d337679a47
nyel-101 <dandevjr@gmail.com>
2025-03-23 23:57:04 +0000

    test(db): fix payload parser

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea155e7c70c346d0a7415c3367e5d83a = () => {
-  return '2025-02-13T00:38:23Z';
+const node_6bb379b330144790bde7b4bf16d23f5d = () => {
+  return '2025-03-23T23:57:04Z';
 };
