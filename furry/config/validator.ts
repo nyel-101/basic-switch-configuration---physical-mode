53394f0cc0154237b93836582502979f315eccca
nyel-101 <dandevjr@gmail.com>
2025-03-25 16:33:52 +0000

    docs(api): refactor auth tokens

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9534d94c1dcc48fbae3d958f65e76b40 = () => {
-  return '2026-05-07T09:07:07Z';
+const node_761308c85a864dd8b4ed742712175bcd = () => {
+  return '2025-03-25T16:33:52Z';
 };
