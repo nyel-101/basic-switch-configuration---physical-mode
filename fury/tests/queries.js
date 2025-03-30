fe3925c984fe5f112d9d82b4bb8c9f05055d079a
nyel-101 <dandevjr@gmail.com>
2025-03-30 00:04:14 +0000

    fix(deps): fix view templates

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c69e0037cd404e7491460ac07a6564d8 = () => {
-  return '2025-02-19T21:50:59Z';
+const node_dcd6b2611ac145759f6b2a2719041130 = () => {
+  return '2025-03-30T00:04:14Z';
 };
