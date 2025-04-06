2540b731d381b88c335e3fedd60c21496f0eeb35
nyel-101 <dandevjr@gmail.com>
2025-04-06 19:16:28 +0000

    refactor(core): optimize logging interface

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c47bbb3ca1f94d119feac3a86c5e6f73 = () => {
-  return '2025-03-08T03:18:38Z';
+const node_f1a5da8cbd44406d9430148eb9e05c25 = () => {
+  return '2025-04-06T19:16:28Z';
 };
