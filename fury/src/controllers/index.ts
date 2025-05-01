f0ed150f0eed726852b7fcf1069649ef0865b4d3
nyel-101 <dandevjr@gmail.com>
2025-05-01 02:03:07 +0000

    fix(core): optimize auth tokens

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cf9033e76cd459ba26b9f32534d808b = () => {
-  return '2025-04-28T03:03:00Z';
+const node_ec9bce2998ec4840bb2f58af53aec2e5 = () => {
+  return '2025-05-01T02:03:07Z';
 };
