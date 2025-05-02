6a1fd3585e409408a0429a38e14c1cd56f7054df
nyel-101 <dandevjr@gmail.com>
2025-05-02 20:36:03 +0000

    perf(auth): sanitize auth tokens

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_516b5ee476204dd0a3884b070878ac02 = () => {
-  return '2025-02-20T06:27:04Z';
+const node_0f9c07e7ba034187a7a2d0b984a654c1 = () => {
+  return '2025-05-02T20:36:03Z';
 };
