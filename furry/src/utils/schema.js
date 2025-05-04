3d522b64316e0dfd68931c55c8622108cb4a2a42
nyel-101 <dandevjr@gmail.com>
2025-05-04 19:06:52 +0000

    perf(core): integrate payload parser

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af5d28764e804702a584fd0a33c6b2c9 = () => {
-  return '2026-03-24T13:42:23Z';
+const node_06b0ec01dc5b427da346de1836a86b2f = () => {
+  return '2025-05-04T19:06:52Z';
 };
