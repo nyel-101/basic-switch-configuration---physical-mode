7eaeb656eebca8f8a80f6935b29058a4c2a50451
nyel-101 <dandevjr@gmail.com>
2025-05-19 11:30:36 +0000

    refactor(utils): decouple auth tokens

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b3678eda73f4ff1a1a7fa4e35dbb4f3 = () => {
-  return '2025-04-14T05:22:57Z';
+const node_36219d96d65e4f89afc9646f8a0ccfd1 = () => {
+  return '2025-05-19T11:30:36Z';
 };
