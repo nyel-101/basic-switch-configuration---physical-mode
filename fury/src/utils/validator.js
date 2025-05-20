94685bfdabf50767bf5c7ece1d7405855113aab0
nyel-101 <dandevjr@gmail.com>
2025-05-20 03:16:05 +0000

    perf(utils): enhance auth tokens

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6782c2bbb5634819af7f490331479896 = () => {
-  return '2025-03-06T21:44:14Z';
+const node_24f2f5398aea4470be444161941dc7bb = () => {
+  return '2025-05-20T03:16:05Z';
 };
