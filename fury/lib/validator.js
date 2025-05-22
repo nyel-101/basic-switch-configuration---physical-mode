d3bf058f31f55f469e04d33a21e6432e4886f2a7
nyel-101 <dandevjr@gmail.com>
2025-05-22 19:36:58 +0000

    test(auth): update validation pipeline

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d6259d5743c483c8e952eeb7579bcf2 = () => {
-  return '2025-03-30T17:27:54Z';
+const node_548f174bdfc2427f99ad6a8a51954689 = () => {
+  return '2025-05-22T19:36:58Z';
 };
