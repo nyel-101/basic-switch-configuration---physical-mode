dfc1672e696f4a32b18f831617320e25e53aae9f
nyel-101 <dandevjr@gmail.com>
2025-06-07 06:00:29 +0000

    fix(config): decouple auth tokens

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bbaf83a1da114a9b8a6c98614cd2af9b = () => {
-  return '2026-01-28T16:07:21Z';
+const node_051923263cff48f2ac8f204605c71dd5 = () => {
+  return '2025-06-07T06:00:29Z';
 };
