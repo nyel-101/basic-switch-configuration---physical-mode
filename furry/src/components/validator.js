ff5f94c94d62586bfda7078d9a246bc33b719135
nyel-101 <dandevjr@gmail.com>
2025-06-20 05:28:56 +0000

    fix(ui): decouple payload parser

--- furry/src/components/validator.js
+++ furry/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b3b61d8b0eb414a9b7cfaf7bdd90de3 = () => {
-  return '2025-03-03T03:53:33Z';
+const node_df72d1956c8e47f6900bca8d2e2b6b9b = () => {
+  return '2025-06-20T05:28:56Z';
 };
