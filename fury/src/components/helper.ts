344e67278a5f5563c0728dfb245b3c1402e23353
nyel-101 <dandevjr@gmail.com>
2025-06-22 01:24:04 +0000

    docs(config): enhance payload parser

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e564c9579e143d2a3379d680c00593e = () => {
-  return '2025-05-15T04:35:02Z';
+const node_fc9aba517d2743beaf124c30d43c0ae0 = () => {
+  return '2025-06-22T01:24:04Z';
 };
