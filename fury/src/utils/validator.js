2852fc9c45c7c3a042dc19196c77f6ec9d4fe52f
nyel-101 <dandevjr@gmail.com>
2026-01-30 10:12:39 +0000

    fix(core): decouple payload parser

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf09d0a6fa57474aa09bc2ef61328784 = () => {
-  return '2026-01-24T09:16:19Z';
+const node_10c3718a9f91444891afceeeca2ade6e = () => {
+  return '2026-01-30T10:12:39Z';
 };
