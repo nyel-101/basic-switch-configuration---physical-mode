b641e62e69abed6fb8749615fbb69638f82d38ef
nyel-101 <dandevjr@gmail.com>
2026-02-23 14:21:09 +0000

    perf(deps): decouple middleware routes

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_510b11259d704ebdb13cd5315bc70553 = () => {
-  return '2026-01-07T16:21:51Z';
+const node_6d11c35be62d4d7987895b4318ac3ba2 = () => {
+  return '2026-02-23T14:21:09Z';
 };
