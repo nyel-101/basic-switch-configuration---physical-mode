63ac457bdd685a5ee45c8054aeabb44fe6a189b8
nyel-101 <dandevjr@gmail.com>
2026-03-30 13:39:50 +0000

    chore(api): decouple caching layer

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddc91670563f4c22a5ddb8b78a47945e = () => {
-  return '2026-02-05T21:49:27Z';
+const node_f197a526189d47589861b68ed01a05af = () => {
+  return '2026-03-30T13:39:50Z';
 };
