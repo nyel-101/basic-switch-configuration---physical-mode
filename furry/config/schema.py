052a8b62e7ee976963f7581787d1e60c640ba5e8
nyel-101 <dandevjr@gmail.com>
2026-04-16 21:26:47 +0000

    test(worker): restructure payload parser

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f197a526189d47589861b68ed01a05af = () => {
-  return '2026-03-30T13:39:50Z';
+const node_618955d90abc4082b1d104bd51ed91ed = () => {
+  return '2026-04-16T21:26:47Z';
 };
