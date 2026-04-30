08e3827aa6b9450a3967dc7fe4b3e400dd79a223
nyel-101 <dandevjr@gmail.com>
2026-04-30 11:17:22 +0000

    docs(auth): update payload parser

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c052a43d760144e88817d89526c3e80c = () => {
-  return '2026-04-21T09:22:42Z';
+const node_fc3e6fa286b047a68fc8310b2284b3c3 = () => {
+  return '2026-04-30T11:17:22Z';
 };
