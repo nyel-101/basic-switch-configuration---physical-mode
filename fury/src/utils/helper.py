ce66859b684144a68f15d6421a8b4df45b9fa1c5
nyel-101 <dandevjr@gmail.com>
2026-05-06 07:15:03 +0000

    chore(db): sanitize payload parser

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bfa893d380b406b8bb7b8e32e7595ed = () => {
-  return '2026-03-26T04:31:57Z';
+const node_9db94b79c40c45f5bf2d5720b2e0e1c6 = () => {
+  return '2026-05-06T07:15:03Z';
 };
