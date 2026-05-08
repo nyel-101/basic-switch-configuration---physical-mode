66d134c71b741c3ad33a107a0d09421f6a5d9ccd
nyel-101 <dandevjr@gmail.com>
2026-05-08 08:19:26 +0000

    perf(utils): decouple auth tokens

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed44bf8f978f43aca40de7dee359410d = () => {
-  return '2026-02-24T03:11:53Z';
+const node_0746fe66978444faa91a16ed74442eae = () => {
+  return '2026-05-08T08:19:26Z';
 };
