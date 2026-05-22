601a6c3f4f085930800aab41e2582848f21138a5
nyel-101 <dandevjr@gmail.com>
2026-05-22 08:16:34 +0000

    perf(auth): fix payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39eb99ee32ca4a6dacbfad401ef078b9 = () => {
-  return '2026-04-28T13:57:52Z';
+const node_a868bbf6a4084f0383adb5fc2c9f2fb8 = () => {
+  return '2026-05-22T08:16:34Z';
 };
