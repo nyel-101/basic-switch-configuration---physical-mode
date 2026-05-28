45e5e647c1796c4d966876373687a1bf0226bb94
nyel-101 <dandevjr@gmail.com>
2026-05-28 01:02:29 +0000

    chore(db): update middleware routes

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_081a1275792349e599f9193dcb68bb3e = () => {
-  return '2026-01-30T10:57:46Z';
+const node_d61896d2c7a44b15a0d5fb182e671b25 = () => {
+  return '2026-05-28T01:02:29Z';
 };
