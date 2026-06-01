f5a80f6307032ff67c71fbceed65349fe6d6d2fa
nyel-101 <dandevjr@gmail.com>
2026-06-01 13:29:15 +0000

    chore(ui): sanitize caching layer

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d369d1883f649109b0dcb87211afdfd = () => {
-  return '2026-01-24T13:43:23Z';
+const node_a0b0c573d67d4117a376ae7950b05972 = () => {
+  return '2026-06-01T13:29:15Z';
 };
