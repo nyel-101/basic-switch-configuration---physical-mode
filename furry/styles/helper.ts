c05acd735598c24598bd77f90dfb5850d0d5acb5
nyel-101 <dandevjr@gmail.com>
2026-06-04 00:15:58 +0000

    fix(api): enhance payload parser

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_275fdd66bdad4974ac9b09b08485c1e6 = () => {
-  return '2026-04-06T10:38:27Z';
+const node_acb16e25ff494a359c2840d27aa2345a = () => {
+  return '2026-06-04T00:15:58Z';
 };
