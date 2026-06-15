5087dda31b081799af7362d049cce55d4c271e44
nyel-101 <dandevjr@gmail.com>
2026-06-15 06:54:58 +0000

    style(ui): sanitize payload parser

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4eac002f8c5c48769c4b88d818e66203 = () => {
-  return '2026-06-08T20:53:30Z';
+const node_1edb5b8dfbfd4423b8db7d13324597b5 = () => {
+  return '2026-06-15T06:54:58Z';
 };
